58a038382f35ef18c7f518cbff265660d97d56e2
sitscholl <tscholl.simon@gmail.com>
2024-12-24 17:53:06 +0100

    created export_sbr function that uses export function of sbr portal to get station data

--- functions/fetch_sbr.py
+++ functions/fetch_sbr.py
@@ -2,10 +2,12 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 import pandas as pd
 import numpy as np
 from io import StringIO
+import datetime
 
 def get_br_stationdata(driver, jahr, station_id = "103", months = np.arange(4, 9), user = None, pwd = None):
 
@@ -73,3 +75,73 @@ def get_br_stationdata(driver, jahr, station_id = "103", months = np.arange(4, 9
     print('SBR Wetterdaten heruntergeladen!')
 
     return(data_concat)
+
+def export_sbr(driver, start, end, station_name, user, pwd):
+
+    ##Validate input dates
+    try:
+        if start != datetime.datetime.strptime(start, "%d.%m.%Y").strftime('%d.%m.%Y'):
+            raise ValueError
+    except ValueError:
+        raise ValueError(f'Start date needs to be in %d.%m.%Y format. Got {start}')
+
+    try:
+        if end != datetime.datetime.strptime(end, "%d.%m.%Y").strftime('%d.%m.%Y'):
+            raise ValueError
+    except ValueError:
+        raise ValueError(f'End date needs to be in %d.%m.%Y format. Got {end}')
+
+    ## Open Browser
+    driver.get('https://www3.beratungsring.org/')
+
+    ##Wait for page to be loaded
+    psource = driver.execute_script("return document.documentElement.outerHTML")
+    time.sleep(2)
+    while True:
+        psource_new = driver.execute_script("return document.documentElement.outerHTML")
+        if psource != psource_new:
+            psource = psource_new
+            time.sleep(2)
+        else:
+            break
+
+    ## Log in (if needed)
+    if driver.find_elements(By.XPATH, '//span[normalize-space()="Login"]'):
+
+        driver.find_element(By.XPATH, '//a[@class="login-link"]').click()
+        ##Insert Email
+        driver.find_element(By.XPATH, '//input[@id="s_username"]').send_keys(user)
+        ##Insert Password
+        driver.find_element(By.XPATH, '//input[@id="s_password"]').send_keys(pwd)
+        ##Press Anmelden
+        driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+
+        print('SBR Anmeldung erfolgreich.')
+
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//span[normalize-space()="Mein SBR"]'))).click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div[1]/div[6]/ul/li[5]/a'))).click()
+    driver.switch_to.window(driver.window_handles[-1])
+
+    driver.get("https://www.beratungsring.org/beratungsring/export_wetterdaten.php?tyid=207&L=0")
+
+    ##Start date
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "datepicker_from"))).clear()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "datepicker_from"))).send_keys(start)
+
+    ##End date
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "datepicker_to"))).clear()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "datepicker_to"))).send_keys(end)
+
+    ##Select station
+    st_select = Select(driver.find_element(By.NAME, 'st_id'))
+    st_select.select_by_visible_text(station_name)
+
+    ##Select Format
+    st_select = Select(driver.find_element(By.NAME, 'ExportFormat'))
+    st_select.select_by_visible_text('CSV-Datei')
+
+    ##Export
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "submit"))).click()
+
+    return(f"{station_name.replace(' ', '_')}.csv")
+
