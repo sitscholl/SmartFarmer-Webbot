565d7c8bad3b1cc652a3c66324fe2a93e7531e04
sitscholl <tscholl.simon@gmail.com>
2025-01-11 13:54:00 +0100

    included headless option also for windows

--- main.py
+++ main.py
@@ -52,10 +52,10 @@ tbl_behandlungsintervall_re = (
 options = Options()
 options.add_argument("--disable-search-engine-choice-screen")
 options.add_argument("--start-maximized")
+options.add_argument('--headless')
 
 if platform.uname().system != 'Windows':
     options.add_argument("--window-size=1920,1080")
-    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--no-gpu')
     options.add_argument('--disable-extensions')
