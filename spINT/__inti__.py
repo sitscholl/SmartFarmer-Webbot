4e1ccf956ad116a9d151dac564160d7526866c0e
sitscholl <tscholl.simon@gmail.com>
2025-03-09 09:41:24 +0100

    renamed package and moved imports to __init__.py

---
+++ spINT/__inti__.py
@@ -0,0 +1,6 @@
+from .init_driver import init_driver
+from .fetch_smartfarmer import fetch_smartfarmer, reformat_sm_data
+from .fetch_sbr import export_sbr, open_sbr_export
+from .google import send_mail, send_sheets
+from .format_tbl import format_tbl
+from .load_data import load_behandlungsintervall, load_regenbestaendigkeit, load_sortenanfaelligkeit
